533f93d185c514b4822520ed832942ebef8ae63a
MasterCode20 <juniorngoumou724@gmail.com>
2024-08-15 23:50:37 +0200

    add vercel analytics module for our app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactForm from './ContactForm';
+import { Analytics } from '@vercel/analytics/react';
 import './ContactForm';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Contacter nous et recevez un Devis personnalisez!!</h1>
       <ContactForm />
+      <Analytics />
     </div>
   );
 }
